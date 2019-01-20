99bb9e682bd62e6630b5860abc216b3c9e97fa18
Sophia Mai <phuongmai001@gmail.com>
2019-01-20 15:53:25 -0800

    map root state to component using connect fn

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,12 @@
-const initState = {};
+const initState = {
+    projects: [
+        {id: 1, title: "Google Map Recommendation", content: "blah blah blah"},
+        {id: 2, title: "Project Management Planner", content: "blah blah blah"},
+        {id: 3, title: "Profile Page", content: "blah blah blah"},
+        {id: 4, title: "Using MongoDB for Data Storage", content: "blah blah blah"},
+        {id: 5, title: "Animal Trading Card", content: "blah blah blah"},
+    ],
+};
 
 const projectReducer = (state = initState, action) => {
     return state;
