3b4b61904a7d5a82fad44b821ca16ec57f53a8c1
Sophia Mai <phuongmai001@gmail.com>
2019-01-21 12:36:25 -0800

    add async call to db to add new project

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,6 +1,22 @@
+import ProjectSummary from "../../components/projects/ProjectSummary";
+
 export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async call to database
-        dispatch({ type: 'CREATE_PROJECT', project });
+
+        const firestore = getFirestore(); // connect to firebase storage
+
+        // link to 'projects' collection in firebase database and add new project
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Test First Name',
+            authorLastName: 'Test Last Name',
+            authorId: 12345,
+            createdAt: new Date(),
+        }).then(() => { // after adding new info to DB, we invoke the dispatch fn
+            dispatch({ type: 'CREATE_PROJECT', project });
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+        });
     };
 };
