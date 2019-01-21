40acd8a8df425c16aabe9473397d2a3cbaa7f3b3
Sophia Mai <phuongmai001@gmail.com>
2019-01-21 11:33:22 -0800

    connect getFirebase and getFirestore to our redux store

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './store/reducers/rootReducer';
 import thunk from 'redux-thunk';
+import { getFirestore } from 'redux-firestore';
+import { getFirebase } from 'react-redux-firebase';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument({
+    getFirebase,
+    getFirestore,
+})));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
