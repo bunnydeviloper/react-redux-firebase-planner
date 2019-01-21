b46f648af97982c95ff819e5a7adbb5b2cc4c02b
Sophia Mai <phuongmai001@gmail.com>
2019-01-21 14:45:17 -0800

    render component based on newly received props

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -4,21 +4,32 @@ import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
 
 const ProjectDetails = (props) => {
-    const id = props.match.params.id;
-    return (
-        <div className="container section project-details">
-            <div className="card z-depth-0">
-                <div className="card-content">
-                    <span className="card-title">Project {id}</span>
-                    <p>Bla bla bla bla bla hahahahhahaha...</p>
-                </div>
-                <div className="card-action lighten-4 grey-text">
-                    <div>Posted by The Sleepy Sophie</div>
-                    <div>19th January, 2pm</div>
+    const { currProject } = props;
+
+    if (currProject) {
+        return (
+            <div className="container section project-details">
+                <div className="card z-depth-0">
+                    <div className="card-content">
+                        <span className="card-title">{currProject.title}</span>
+                        <p>{currProject.content}</p>
+                    </div>
+                    <div className="card-action lighten-4 grey-text">
+                        <div>Posted by {currProject.authorFirstName} {currProject.authorLastName}</div>
+                        <div>19th January, 2pm</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div className="container center">
+                <p>Loading project...</p>
+            </div>
+        );
+    }
+}
+
 const mapStateToProps = (state, ownProps) => {
     const id = ownProps.match.params.id;
     const projects = state.firestore.data.projects;
