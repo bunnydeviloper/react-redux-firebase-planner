e87a9d55f1f151068bc3e4bf7dce29233e713f4e
Sophia Mai <phuongmai001@gmail.com>
2019-01-20 18:49:28 -0800

    project content will be in project details instead of summary

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,7 +5,6 @@ const ProjectSummary = ({ project }) => {
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title">{project.title}</span>
-                <p>{project.content}</p>
                 <p>Posted by Sleepy Kitty</p>
                 <p className="grey-text">Jan 16th, 2019</p>
             </div>
