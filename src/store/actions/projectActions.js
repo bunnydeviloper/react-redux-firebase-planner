778bf78c1ee912807c81e3c6da2b598bb4b4ccd3
Sophia Mai <phuongmai001@gmail.com>
2019-01-22 00:06:18 -0800

    update createProject fn

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -4,12 +4,15 @@ export const createProject = (project) => {
 
         const firestore = getFirestore(); // connect to firebase storage
 
+        const profile = getState().firebase.profile;
+        const authorId = getState().firebase.auth.uid;
+
         // link to 'projects' collection in firebase database and add new project
         firestore.collection('projects').add({
             ...project,
-            authorFirstName: 'Test First Name',
-            authorLastName: 'Test Last Name',
-            authorId: 12345,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorId: authorId,
             createdAt: new Date(),
         }).then(() => { // after adding new info to DB, we invoke the dispatch fn
             dispatch({ type: 'CREATE_PROJECT', project });
