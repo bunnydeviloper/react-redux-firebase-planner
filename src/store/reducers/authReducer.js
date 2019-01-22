d4abc7e99fb2a7b66e56a530b315e1dfa8784be4
Sophia Mai <phuongmai001@gmail.com>
2019-01-21 23:13:03 -0800

    create signup action and connect to reducer

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -16,6 +16,16 @@ const authReducer = (state = initState, action) => {
             };
         case 'SIGNOUT':
             return state;
+        case 'SIGNUP_SUCCESS':
+            return {
+                ...state,
+                authError: null,
+            };
+        case 'SIGNUP_ERROR':
+            return {
+                ...state,
+                authError: action.err.message,
+            };
         default:
             return state;
     }
